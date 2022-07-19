5bab40a89005d0e6af6051a0d3a6dbe46345ada8
Matthew Larkin <matthewlarkin8@gmail.com>
2022-07-19 16:05:40 -0400

    Created tests for all three functions.

---
+++ test_series.py
@@ -0,0 +1,59 @@
+from series import fibonacci
+
+#test fibonacci function
+def test_fibonacci_input_1():
+    fibonacci(1)
+
+def test_fibonacci_input_2():
+    fibonacci(2)
+
+def test_fibonacci_input_3():
+    fibonacci(3)
+
+def test_fibonacci_input_5():
+    fibonacci(5)
+
+def test_fibonacci_input_10():
+    fibonacci(10)
+
+
+# # test lucas function
+# def test_lucas_input_1():
+#     lucas(1)
+#
+#
+# def test_lucas_input_2():
+#     lucas(2)
+#
+#
+# def test_lucas_input_3():
+#     lucas(3)
+#
+#
+# def test_lucas_input_5():
+#     lucas(5)
+#
+#
+# def test_lucas_input_10():
+#     lucas(10)
+#
+#
+# # test sum_series function
+# def test_sum_series_input_1():
+#     sum_series(1)
+#
+#
+# def test_sum_series_input_2():
+#     sum_series(2)
+#
+#
+# def test_sum_series_input_3():
+#     sum_series(3)
+#
+#
+# def test_sum_series_input_5():
+#     sum_series(5)
+#
+#
+# def test_sum_series_input_10():
+#     sum_series(10)
