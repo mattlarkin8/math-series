70c6ecdc223bf8518930a9cc4b705c5db8991de8
Matthew Larkin <matthewlarkin8@gmail.com>
2022-07-19 20:49:00 -0400

    Created tests for sum_series. Made two separate sets of tests to check the function with fibonacci numbers and lucas numbers. Got the function working properly for fibonacci; passes all tests.

--- test_series.py
+++ test_series.py
@@ -1,4 +1,4 @@
-from series import fibonacci, lucas
+from series import fibonacci, lucas, sum_series
 import pytest
 
 
@@ -34,33 +34,95 @@ import pytest
 #     assert actual == expected
 
 
-#test fibonacci function
-def test_lucas_input_0():
-    actual = lucas(0)
-    expected = 2
+#test lucas function
+# def test_lucas_input_0():
+#     actual = lucas(0)
+#     expected = 2
+#     assert actual == expected
+#
+# def test_lucas_input_1():
+#     actual = lucas(1)
+#     expected = 1
+#     assert actual == expected
+#
+# def test_lucas_input_2():
+#     actual = lucas(2)
+#     expected = 3
+#     assert actual == expected
+#
+# def test_lucas_input_3():
+#     actual = lucas(3)
+#     expected = 4
+#     assert actual == expected
+#
+# def test_lucas_input_5():
+#     actual = lucas(5)
+#     expected = 11
+#     assert actual == expected
+#
+# def test_lucas_input_10():
+#     actual = lucas(10)
+#     expected = 123
+#     assert actual == expected
+
+#test sum_series function with fibonacci numbers
+def test_sum_series_for_fib_0():
+    actual = sum_series(0)
+    expected = 0
     assert actual == expected
 
-def test_lucas_input_1():
-    actual = lucas(1)
+def test_sum_series_for_fib_1():
+    actual = sum_series(1)
     expected = 1
     assert actual == expected
 
-def test_lucas_input_2():
-    actual = lucas(2)
-    expected = 3
+def test_sum_series_for_fib_2():
+    actual = sum_series(2)
+    expected = 1
     assert actual == expected
 
-def test_lucas_input_3():
-    actual = lucas(3)
-    expected = 4
+def test_sum_series_for_fib_3():
+    actual = sum_series(3)
+    expected = 2
     assert actual == expected
 
-def test_lucas_input_5():
-    actual = lucas(5)
-    expected = 11
+def test_sum_series_for_fib_5():
+    actual = sum_series(5)
+    expected = 5
     assert actual == expected
 
-def test_lucas_input_10():
-    actual = lucas(10)
-    expected = 123
+def test_sum_series_for_fib_10():
+    actual = sum_series(10)
+    expected = 55
     assert actual == expected
+
+#test sum_series function with lucas numbers
+def test_sum_series_for_lucas_0():
+    actual = sum_series(0, 2, 1)
+    expected = 2
+    assert actual == expected
+
+# def test_sum_series_for_lucas_1():
+#     actual = sum_series(1, 2, 1)
+#     expected = 1
+#     assert actual == expected
+#
+# def test_sum_series_for_lucas_2():
+#     actual = sum_series(2, 2, 1)
+#     expected = 3
+#     assert actual == expected
+#
+# def test_sum_series_for_lucas_3():
+#     actual = sum_series(3, 2, 1)
+#     expected = 4
+#     assert actual == expected
+#
+# def test_sum_series_for_lucas_5():
+#     actual = sum_series(5, 2, 1)
+#     expected = 11
+#     assert actual == expected
+#
+# def test_sum_series_for_lucas_10():
+#     actual = sum_series(10, 2, 1)
+#     expected = 123
+#     assert actual == expected
